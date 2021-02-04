a199eef0a184a172a4739f6d5b0715a52d3762c8
amitgade <amitgaade@gmail.com>
2021-02-04 19:05:39 +0530

    action-Remove commented code

--- src/test/java/hashtables/ChainedHashTableTest.java
+++ src/test/java/hashtables/ChainedHashTableTest.java
@@ -1,11 +1,9 @@
 package hashtables;
 
-import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
-import pojos.Employee;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 /**
  * Created by amitgade on 01-Feb-2021
@@ -27,27 +25,24 @@ public class ChainedHashTableTest {
         hashTable.put("Henry", "Ford"); // this will cause collision
         hashTable.put("Shiv", "Khera"); // this will cause collision
 
-//        assertEquals("John", hashTable.get("Doe"));
-//        assertEquals("Jack", hashTable.get("Sparrow"));
-//        assertEquals("Nikola", hashTable.get("Tesla"));
-//        assertEquals("Henry", hashTable.get("Ford"));
+        assertEquals("John", hashTable.get("Doe"));
+        assertEquals("Jack", hashTable.get("Sparrow"));
+        assertEquals("Nikola", hashTable.get("Tesla"));
+        assertEquals("Henry", hashTable.get("Ford"));
 
         System.out.println(hashTable);
     }
 
     @Test
     public void remove() {
-//        hashTable.put("John", "Doe");
-//        hashTable.put("Jack", "Sparrow");
-//        hashTable.put("Nikola","Tesla");
-//        hashTable.put("Henry", "Ford"); // this will cause collision
-//        System.out.println(hashTable);
+        hashTable.put("John", "Doe");
+        hashTable.put("Jack", "Sparrow");
+        hashTable.put("Nikola","Tesla");
+        hashTable.put("Henry", "Ford"); // this will cause collision
+        hashTable.put("Shiv", "Khera"); // this will cause collision
 
-//        assertEquals("John", hashTable.get("Doe"));
-//        assertEquals("Jack", hashTable.get("Sparrow"));
-//        assertEquals("Nikola", hashTable.get("Tesla"));
-//        assertEquals("Henry", hashTable.get("Ford"));
+        assertEquals("John", hashTable.get("Doe"));
 
-//        System.out.println(hashTable);
+        System.out.println(hashTable);
     }
 }
